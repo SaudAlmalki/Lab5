25f54fac269222c2ba89e4964fa292e9e54dbc2f
SaudAlmalki <159236742+SaudAlmalki@users.noreply.github.com>
2024-03-14 19:40:52 +0300

    Add files via upload

--- myproject1/myproject/myapp/views.py
+++ myproject1/myproject/myapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Person
+from .forms import PersonForm
+
+people = []
+
+def add_person(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            person = Person(username=username, password=password)
+            people.append(person)
+            return HttpResponseRedirect('/')
+    else:
+        form = PersonForm()
+    return render(request, 'add_person.html', {'form': form})
+
+def show_people(request):
+    return render(request, 'show_people.html', {'people': people})
+
+
 
 # Create your views here.
